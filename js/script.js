1009ce5df99392d3f6de2a2c344489d215172fa3
Manoel Oliveira <manutt577@gmail.com>
2023-12-18 02:41:11 -0300

    Tooltip refatorada

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from './module/scroll-suave.js';
 import Accordion from './module/accordion.js';
 import TabNav from './module/tabnav.js';
 import Modal from './module/modal.js';
-import initTooltip from './module/tooltip.js';
+import Tooltip from './module/tooltip.js';
 import initDropdownMenu from './module/dropdown-menu.js';
 import initMenuMobile from './module/menu-mobile.js';
 import initFuncionamento from './module/funcionamento.js';
@@ -22,7 +22,9 @@ tabNav.init();
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
-initTooltip();
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
